da8aab4a067d6688b15cf9259a5e65cdd577bb70
tapankumar23 <sabat.tapan@gmail.com>
2018-09-09 12:43:01 +0800

    Classpath.txt added

---
+++ sample_java9/src/com.aswm.dev/com/awsm/dev/cptest/first/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.awsm.dev.cptest.first;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("This the first Hello World Class !!!!!");
+		
+		Thread.sleep(500000);
+	}
+}
---
+++ sample_java9/src/com.aswm.dev/com/awsm/dev/cptest/second/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.awsm.dev.cptest.second;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("This the Second Hello World Class !!!!!");
+		
+		Thread.sleep(500000);
+	}
+}
